fe917ec755f80f8f56e802740279f267106aed82
Joanne Kitson <joanne@sundogtools.com>
2018-08-29 19:42:40 +0100

    adding cmd.py and board.py changes

--- battleships/board.py
+++ battleships/board.py
@@ -5,10 +5,10 @@ class board ():
 
     def __init__(self):
         self.grid = np.zeros((10,10),dtype=int)
+        self.ship = []
 
-
-    def add_ship (self):
-        pass
+    def add_ship (self,x,y):
+        self.ship.append((x,y))
 
     def potshot(self,x,y):
         pass
--- battleships/cmd.py
+++ battleships/cmd.py
@@ -1,2 +1,13 @@
+from battleships.board import board
+
 def main():
-    pass
+    player1 = board()
+    player2 = board()
+
+    print("PLAYER1 ENTER YOUR SHIPS;")
+    for ship in ships:
+        location = input("nmmmm")
+        # get x and y from here
+        x = ..
+        y = ...
+        player1.add_ship(x, y)
