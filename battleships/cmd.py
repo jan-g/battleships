c0ced902ea627637f54c71dc815a7a5b98ce5156
Jonathan Few <jonathan.few@simpleweb.co.uk>
2018-08-29 20:03:52 +0100

    Got code working

--- battleships/cmd.py
+++ battleships/cmd.py
@@ -1,26 +1,32 @@
-from battleships.board import board
+from board import board
 
 
-ships = [["Carrier"],
-		["Battleship"],
-		["Cruiser"],
-		["Submarine"],
-		["Destroyer"]]
-
-def main():
-    player1 = get_ships("player1")
-    player2 = get_ships("player1")
-
+ships = ["Carrier",
+		"Battleship",
+		"Cruiser",
+		"Submarine",
+		"Destroyer"]
 
 
 def get_ships(name):
-    print("{}".format(name))
+    print("{} pick your ships!".format(name))
     player = board()
     for ship in ships:
-        locationx = input("enter x position of {}".format(ship))
-        locationy = input("enter y position of {}".format(ship))
+        locationx = input("enter x position of {}: ".format(ship))
+        locationy = input("enter y position of {}: ".format(ship))
         # get x and y from here
         x = int(locationx)
         y = int(locationy)
         player.add_ship(x, y)
-        return player
+
+    return player
+
+
+def main():
+    player1 = get_ships("Player 1")
+    player2 = get_ships("Player 2")
+    print(player1)
+    print(player2)
+
+if __name__ == "__main__":
+	main()
