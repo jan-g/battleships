528f7b6e851c48342986f3151f4b146555b216a5
Joanne Kitson <joanne@sundogtools.com>
2018-08-29 19:27:15 +0100

    adding board.py

---
+++ battleships/board.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class board ():
+
+    def __init__(self):
+        self.grid = np.zeros((10,10),dtype=int)
+
+
+    def add_ship (self):
+        pass
+
+    def potshot(self,x,y):
+        pass
+
+    def defeated(self):
+        return False
+
+
